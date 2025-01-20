7aae2d048216bd284a3e40d58e31b863a89314b0
Kush Vekeria <95439093+kvekeria@users.noreply.github.com>
2025-01-20 09:14:49 -0500

    next commit

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+import random
 
 app = FastAPI()
 
-@app.get('/')
-def root():
-    return {"message": "welcome to my api"}
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "I like pizza.", "id": 2}]
+
+@app.get('/posts')
+def get_posts():
+    return {"posts": my_posts}
 
 @app.get('/')
-def get_posts():
-    return {"data": "This is your post"}
+def home():
+    return {"MESSAGE": "Welcome to my Test API"}
+
+@app.post("/posts")
+def create_posts(post: Post):
+    new_post = post.dict()
+    new_post['id'] = random.randint(1,100000000000)
+    my_posts.append(new_post)
+    return {"SUCCESS": new_post}
+#title, str and content, str
